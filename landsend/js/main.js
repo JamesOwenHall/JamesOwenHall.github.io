6e8ed0b98ce646f66f762ebcf832750f97aa072b
James Hall <james_hall@live.ca>
2015-05-14 17:03:10 -0400

    Removed landsend

--- landsend/js/main.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    var aboutToggle = document.getElementById('about-toggle');
-    var aboutMenu = document.getElementById('about-menu');
-    NavMenu(aboutToggle, aboutMenu);
-};
